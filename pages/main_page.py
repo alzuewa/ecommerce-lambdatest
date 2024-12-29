35455bdf80bab165587ae0bfcbbfb132b8493829
Albina <albizuewa@gmail.com>
2024-12-29 11:00:06 -0800

    Change selector

--- pages/main_page.py
+++ pages/main_page.py
@@ -7,7 +7,7 @@ class MainPage:
     def __init__(self):
         self.url = '/'
 
-        self.success_popup = browser.element('#notification-box-top')
+        self.success_popup = browser.element('.toast')
         self.cart_icon = browser.element('.cart-icon')
         self.cart_drawer = browser.element('#cart-total-drawer')
         self.edit_cart_button = browser.element('[data-id="217850"] .btn-primary')
