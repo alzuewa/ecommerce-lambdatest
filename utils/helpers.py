dd6e829976d454403ded5a758ac5a79f5a8fa4b2
Albina <albizuewa@gmail.com>
2024-12-29 21:42:39 -0800

    Change format approach

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,5 +1,4 @@
 import json
-import locale
 from decimal import Decimal
 
 from requests import Response
@@ -7,10 +6,9 @@ from requests import Response
 
 def get_total_cart_price(response: Response) -> Decimal:
     string_with_price = json.loads(response.text)['total']
-    price = string_with_price.split('- ')[1][1:].replace(',', '')
+    price = string_with_price.split('$')[1].replace(',', '')
     return Decimal(price)
 
 
 def get_price_repr(price) -> str:
-    locale.setlocale(locale.LC_NUMERIC, 'en_US')
-    return locale.format_string(f='%.2f', val=price, grouping=True)
+    return f'{price:,}'
